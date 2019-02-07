ecbf1e0f1de89dcfe7bec65429f29f0eb75540e3
RoganAlam <45163411+RoganAlam@users.noreply.github.com>
2019-02-07 17:08:06 +0100

    Add files via upload

---
+++ Opdracht 2.4/Opdracht 2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Opdracht_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, input;
+            DateTime dateOfBirth;
+            Console.WriteLine("What is your name");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your date of birth");
+            dateOfBirth = Convert.ToDateTime( Console.ReadLine());
+            TimeSpan age = DateTime.Now.Subtract(dateOfBirth);
+            Console.WriteLine("Hello " + name + " you are " + age.Days/365 + " years old");
+            Console.ReadLine();
+        }
+    }
+}
